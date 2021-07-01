d9ceaeeab54033e05e2772efe532239dc5759e4e
黄高乐 <huanggaole@bytedance.com>
2021-07-01 18:25:59 +0800

    core: 初始化一个Unity项目，可以一键生成特定尺寸的地图。

---
+++ Assets/Scripts/Example/PlainPCG.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlainPCG: MonoBehaviour
+{
+    public GameObject ground_grass;
+
+    public int mapWidth;
+    public int mapHeight;
+
+    public void GenerateMap(){
+        // 使用算法生成地图数组
+        int[,] map = PlainPCG.GeneratePlainMap(mapWidth, mapHeight);
+        // 根据地图数组建模游戏场景
+        this.RenderMap(map);
+    }
+
+    public void RenderMap(int[,] map){
+        // 首先把之前已经生成的地图完全清除
+        int childCount = this.gameObject.transform.childCount;
+        for (int i = childCount - 1; i >=0; i-- )
+        {
+            DestroyImmediate(this.gameObject.transform.GetChild(i).gameObject);
+        }
+        // 根据地图数据装配新地图
+        for(int y = 0; y < mapHeight; y++){
+            for(int x = 0; x < mapWidth; x++){
+                if(map[x,y] == 1){
+                    Vector3 position = new Vector3(x, 0, y);
+                    Quaternion rotation = new Quaternion();
+                    Instantiate(this.ground_grass, position, rotation, this.gameObject.transform);
+                }
+            }
+        }
+    }
+
+    public static int[,] GeneratePlainMap(int mapWidth, int mapHeight){
+        int[,] plainMap = new int[mapWidth, mapHeight];
+        for(int y = 0; y < mapHeight; y++){
+            for(int x = 0; x < mapWidth; x++){
+                plainMap[x,y] = 1;
+            }
+        }
+        return plainMap;
+    }
+}
---
+++ Assets/Scripts/Example/PlainPCGEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PlainPCG))]
+public class PlainPCGEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PlainPCG plainPCG = (PlainPCG)target;
+        if(GUILayout.Button("生成地图")){
+            plainPCG.GenerateMap();
+        }
+    }
+}
